02b0cd55b32e20718da7d0938baee9d27ad97f78
nductrung213 <nguyenductrung.ag@gmail.com>
2016-09-24 10:48:29 +0700

    Pick an element using findAny

--- src/main/java/com/me/examples/java8/functional/programming/java/PickAnElement.java
+++ src/main/java/com/me/examples/java8/functional/programming/java/PickAnElement.java
@@ -5,10 +5,27 @@
  */
 package com.me.examples.java8.functional.programming.java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  *
  * @author trung
  */
 public class PickAnElement {
-    
+
+    private static final Function<String, Predicate<String>> START_WITH_LETTER = letter -> name -> name.startsWith(letter);
+
+    public static void main(String[] args) {
+        final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+        final String letter = "N";
+
+        Optional<String> personStartWithN = friends.stream()
+                .filter(START_WITH_LETTER.apply(letter)).findAny();
+        System.out.println(String.format("A name starting with %s: %s",
+                letter, personStartWithN.orElse("No name found")));
+    }
 }
